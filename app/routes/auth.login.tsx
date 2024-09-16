f0b4770441cfbf4879d6d17766860480088abb70
justmedev <ilja.ragnar@gmail.com>
2024-09-16 12:06:58 +0200

    remove error string at wrong pos

--- app/routes/auth.login.tsx
+++ app/routes/auth.login.tsx
@@ -32,8 +32,6 @@ export default function Login() {
   return (
     <>
       <Card title="Login">
-        {data?.errors.username ?? ""}
-
         <Form method="post">
           <FormField name="username" label="Username" errorHint={data?.errors.username} className="mb-1 w-full"/>
 
