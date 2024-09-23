9db6b927ef647b0c179f9a73c6af7bdf317b51ee
justmedev <ilja.ragnar@gmail.com>
2024-09-23 08:38:32 +0200

    style

--- app/routes/protected.home.tsx
+++ app/routes/protected.home.tsx
@@ -3,9 +3,7 @@ import { isJWTValid } from "~/.server/auth";
 import { Form, redirect } from "@remix-run/react";
 import { destroySession, getSession } from "~/sessions";
 
-export const loader = async ({
-                               request,
-                             }: LoaderFunctionArgs) => {
+export const loader = async ({ request }: LoaderFunctionArgs) => {
   const session = await getSession(request.headers.get("Cookie"));
   if (!session.has("jwt") || !isJWTValid(session.get("jwt") ?? "")) {
     throw redirect("/auth/login")
@@ -31,10 +29,12 @@ export const action = async ({
 export default function ProtectedHome() {
   return (
     <>
-      <div>This page is protected and shall not be accessed by non-logged-in users!</div>
-      <Form method="post">
-        <button className="btn">Logout</button>
-      </Form>
+      <div className="flex flex-col gap-2 justify-center items-center h-full">
+        <div>This page is protected and shall not be accessed by non-logged-in users!</div>
+        <Form method="post">
+          <button className="btn shadow-[0_0_160px_-15px_rgba(0,0,0,0.7)] hover:shadow-none shadow-yellow-50">Logout</button>
+        </Form>
+      </div>
     </>
   );
 }
